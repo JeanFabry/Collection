b40507cafb6bbc66b1c848bbade273f3ea93bc10
JeanFabry <jeanfabry1435@gmail.com>
2020-11-12 16:47:15 +0100

    Project done, except for details (link place)

--- collection.js
+++ collection.js
@@ -1,25 +1,49 @@
 const collection = [{
         title: 'Westworld',
         author: 'Jonathan Nolan',
-        category: {'Science-Fiction', 'AI & Robots'},
+        category: [{
+                Name: 'Science-Fiction',
+                Style: "tag is-dark mr-2"
+            },
+            {
+                Name: 'AI & Robots',
+                Style: "tag is-link"
+            }
+        ],
         link: 'https://www.youtube.com/watch?v=9BqKiZhEFFw&ab_channel=WarnerBros.UK%26Ireland',
         description: "Un parc d'attractions peuplé de robots propose aux visiteurs de se replonger dans plusieurs époques. Lancés dans l'ouest sauvage, deux amis se retrouvent plongés en plein cauchemar quand l'un des androïdes se détraque et les prend en chasse.",
-        image:'./Images/westworld.jpg',   
+        image: './Images/westworld.jpg',
     },
 
     {
         title: 'Final Space',
         author: 'Olan Rogers',
-        category: ['Comédie', 'Animation'],
+        category: [{
+                Name: 'Comédie',
+                Style: "tag is-success mr-2"
+            },
+            {
+                Name: 'Animation',
+                Style: "tag is-warning"
+            }
+        ],
         link: 'https://www.youtube.com/watch?v=Fy2wlCa31-M&ab_channel=OlanRogers',
         description: "l'astronaute Gary et son acolyte Mooncake le destructeur de planètes, voyagent dans l'espace pour trouver où l'univers s'arrête réellement.",
-        image:'./Images/final space.jpg', 
+        image: './Images/final space.jpg',
     },
 
     {
         title: 'Breaking Bad',
         author: 'Mark Johnson',
-        category: ['Crime', 'Drame'],
+        category: [{
+                Name: 'Crime',
+                Style: "tag is-danger mr-2"
+            },
+            {
+                Name: 'Drame',
+                Style: "tag is-black"
+            }
+        ],
         link: 'https://www.youtube.com/watch?v=HhesaQXLuRY&ab_channel=TrailerBlend',
         description: "Walter White, 50 ans, est professeur de chimie dans un lycée du Nouveau-Mexique. Pour réunir de l'argent afin de subvenir aux besoins de sa famille, Walter met ses connaissances en chimie à profit pour fabriquer et vendre du crystal meth.",
         image: './Images/breaking bad.jpg',
@@ -28,120 +52,191 @@ const collection = [{
     {
         title: 'Chernobyl',
         author: 'Craig Mazin',
-        category: ['Drame', 'Historique'],
+        category: [{
+                Name: 'Drame',
+                Style: "tag is-black mr-2"
+            },
+            {
+                Name: 'Historique',
+                Style: "tag is-info"
+            }
+        ],
         link: 'https://www.youtube.com/watch?v=s9APLXM9Ei8&feature=emb_title',
         description: "26 avril 1986, l'histoire vraie de la pire catastrophe causée par l'homme etde ceux qui ont sacrifié leur vie pour sauver l'Europe du drame.",
-        image:'./Images/Chernobyl.jpg', 
+        image: './Images/Chernobyl.jpg',
     },
 
 
     {
         title: 'Sex Education',
         author: 'Laurie Nunn',
-        category: ['Comédie', 'Sexualité'],
+        category: [{
+                Name: 'Comédie',
+                Style: "tag is-success mr-2"
+            },
+            {
+                Name: 'Sexualité',
+                Style: "tag is-danger"
+            }
+        ],
         link: 'https://www.youtube.com/watch?v=Hd2ldTR-WpI&ab_channel=RottenTomatoesTV',
         description: "La rebelle Maeve entraîne Otis, un ado vierge mais dont la mère est sexologue, dans la création d'une cellule de thérapie sexuelle clandestine au sein de leur lycée.",
-        image:'./Images/sex education.jpg',
+        image: './Images/sex education.jpg',
     },
 
     {
         title: 'Sherlock',
         author: 'Sue Vertue',
-        category: ['Crime', 'Sherlock'],
+        category: [
+            {
+                Name: 'Série à énigme',
+                Style: "tag is-primary mr-2"
+            },
+            {
+                Name: 'crime',
+                Style: "tag is-danger"
+            }
+        ],
         link: 'https://www.youtube.com/watch?v=xK7S9mrFWL4&ab_channel=TheEmptyHearse',
         description: "Dans cette nouvelle adaptation des fameuses intrigues d'Arthur Conan Doyle, l'excentrique détective recherche des indices dans les rues de Londres.",
-        image:'./Images/westworld.jpg',
+        image: './Images/Sherlock.jpg',
     },
 
     {
         title: 'When they see us',
         author: 'Ava DuVernay',
-        category: ['Drame', 'Historique'],
+        category: [  
+            {
+                Name: 'Drame',
+                Style: "tag is-black mr-2"
+            },
+            {
+                Name: 'Historique',
+                Style: "tag is-info"
+            }
+        ],
         link: 'https://www.youtube.com/watch?v=KyIrJeK2DKY&ab_channel=KinoCheckInternational',
         description: "Cette série expose l'effondrement du système judiciaire pénal américain lors de l'affaire Central Park Five, une affaire traitant d'une joggeuse de 28 ans agressée et violée en 1989 à Central Park.",
-        image:'./Images/westworld.jpg', 
+        image: './Images/wtsu.jpg',
     },
 
     {
         title: 'Game of thrones',
         author: 'George R. R. Martin',
-        category: ['Héroïque Fantaisie', 'Aventure'],
+        category: [  
+            {
+                Name: 'Héroïque Fantaisie',
+                Style: "tag is-light mr-2"
+            },
+            {
+                Name: 'Aventure',
+                Style: "tag is-success"
+            }
+        ],
         link: 'https://www.youtube.com/watch?v=gcTkNV5Vg1E&ab_channel=VERITASERUMUK',
         description: "Neuf familles nobles rivalisent pour le contrôle du Trône de Fer dans les sept royaumes de Westeros. Pendant ce temps, des anciennes créatures mythiques oubliées reviennent pour faire des ravages.",
-        image:'./Images/westworld.jpg',
+        image: './Images/got.jpg',
     },
 
     {
         title: 'How I met your mother',
         author: 'Carter Bays',
-        category: ['Comédie', 'Sitcom'],
+        category: [  
+            {
+                Name: 'Comédie',
+                Style: "tag is-success mr-2"
+            },
+            {
+                Name: 'Sitcom',
+                Style: "tag is-primary"
+            }
+        ],
         link: 'https://www.youtube.com/watch?v=aJtVL2_fA5w&ab_channel=CokeNpopcorn',
         description: "La série débute en 2030, lorsque Ted Mosby raconte à ses deux enfants comment il a rencontré leur mère. Il se remémore ses jeunes années, et le pilote fait place aux souvenirs de Ted en 2005, où il apprend que son meilleur ami Marshall Eriksen va demander à Lily Aldrin de l'épouser. Ted se demande quand il rencontrera sa future épouse. Et c'est ainsi que commence l'incroyable et très longue histoire de Ted, jusqu'à sa rencontre avec la fameuse mère.",
-        image:'./Images/westworld.jpg', 
+        image: './Images/himym.jpg',
     },
 
     {
         title: 'Peaky Blinders',
         author: 'Katie Swinden',
-        category: ['Drame', 'Crime'],
+        category: [  
+            {
+                Name: 'Drame',
+                Style: "tag is-black mr-2"
+            },
+            {
+                Name: 'Crime',
+                Style: "tag is-danger"
+            }
+        ],
         link: 'https://www.youtube.com/watch?v=oVzVdvGIC7U&ab_channel=Alextvshows',
         description: "Birmingham, en 1919. Un gang familial règne sur un quartier de la ville : les Peaky Blinders, ainsi nommés pour les lames de rasoir qu'ils cachent dans la visière de leur casquette.",
-        image:'./Images/westworld.jpg', 
+        image: './Images/peaky-blinders.jpg',
     },
 ]
 
 
 const body = document.querySelector('section');
 body.innerHTML = '';
-body.className= 'container my-4'
-const divColA= document.createElement("div");
-divColA.className="columns";
-// div.style.display.flex;
-    // div.style.width ="300px";
-    // div.style.flexDirection="row";
+body.className = 'container my-4'
+const divColA = document.createElement("div");
+divColA.className = "columns is-flex-wrap-wrap";
+// divCola.style.display = "flex";
+// div.style.width ="300px";
+// div.style.flexDirection="row";
 
 
 for (let element of collection) {
-    
 
     const div = document.createElement("div");
     div.className = 'column is-3';
-    
+
 
     const card = document.createElement("div");
     card.className = 'card';
+    card.style.boxShadow= '1px 1px 10px grey';
 
     const image = document.createElement("img");
-    image.src=element.image;
+    image.src = element.image;
 
     const title = document.createElement("h1");
-    title.className='title is-4';
+    title.className = 'title is-4 mt-2';
     title.textContent = element.title;
 
     const author = document.createElement('h2');
     author.innerText = element.author;
+    author.className="subtitle is-6 my-3";
 
-
-    const tag1 = document.createElement('span');
-    tag1.innerText = element.category[0];
-    // tags.className = element.classtag;}
-    
     const description = document.createElement("p");
     description.textContent = element.description;
 
-    const link = document.createElement("a");
-    link.textContent= element.link;
+    const footer = document.createElement("footer")
+    footer.className = "card-footer"
+
+    const youtube = document.createElement("a")
+    youtube.className = "card-footer-item fas fa-2x"
+    youtube.href = element.link
+    youtube.target = "_blank"
+
+    const lien = document.createElement("i")
+    lien.className = "fab fa-youtube";
 
     card.appendChild(image);
     card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(tag1);
-    card.appendChild(description);
-    card.appendChild(link);
 
+    for (let tag of element.category) {
+        const tag1 = document.createElement('span');
+        tag1.innerText = tag.Name;
+        tag1.className = tag.Style;
+        card.appendChild(tag1);
+    }
+    card.appendChild(author);
+    card.appendChild(description);
+    footer.appendChild(youtube);
+    youtube.appendChild(lien);
+    card.appendChild(footer);
     div.appendChild(card);
 
     divColA.appendChild(div);
     body.appendChild(divColA);
 }
-
